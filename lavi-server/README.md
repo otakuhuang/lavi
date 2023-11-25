78fa9308e84a3e5317e7e23bef6e62e9ad553c46
otakuhuang <shyxiaopang@gmail.com>
2023-11-26 03:08:08 +0800

    feature: init server

---
+++ lavi-server/README.md
@@ -0,0 +1,2 @@
+# Lavi Server
+
---
+++ lavi-server/src/main/java/top/otakuhuang/laviserver/LaviServerApplication.java
@@ -0,0 +1,13 @@
+package top.otakuhuang.laviserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaviServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaviServerApplication.class, args);
+    }
+
+}
