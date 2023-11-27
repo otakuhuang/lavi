320742c07c544db5a51e95e0fe0e34bd5aca5ffc
otakuhuang <shyxiaopang@gmail.com>
2023-11-28 01:11:32 +0800

    feature: 配置实体字段自动填充

---
+++ lavi-server/src/main/java/top/otakuhuang/laviserver/config/MyBatisPlusMetaObjectHandler.java
@@ -0,0 +1,25 @@
+package top.otakuhuang.laviserver.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author otakuhuang
+ * @date 2023/11/28 上午12:57
+ */
+@Component
+public class MyBatisPlusMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime::now, LocalDateTime.class);
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime::now, LocalDateTime.class);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime::now, LocalDateTime.class);
+    }
+}
